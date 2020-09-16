a82d9b73abeaa84bbc0d516a915e7b019254443d
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2020-09-16 09:18:43 -0400

    added parallelizability

--- process_covers.py
+++ process_covers.py
@@ -26,21 +26,28 @@ def foldImages(list_of_images):
 # If max_part is 0, it will search for all
 # files that fit this format. Otherwise, it
 # will process at most X many 
-def main(naming, directory, AC, part_start=0, part_end=MAX_PARTS):
+def main(naming, directory, AC, part_start=0, part_end=MAX_PARTS, instance=0):
+    images_dir = 'images' if instance == 0 else 'images{}'.format(instance)
+    plaintext_dir = 'plaintext' if instance == 0 else 'plaintext{}'.format(instance)
+
     regex = re.compile(naming + r'No_' + str(AC).zfill(3) + r'PartNo_(\d{3}).pdf')
     directory = os.getcwd() if directory == '' else directory
 
     i = 0
     f_array = []
+    print('Dumping images', end='')
     for root, dirs, files in os.walk(directory):
         for f in files:
             m = regex.match(f)
             if m and is_between(part_start, int(m.group(1)), part_end):
+                print('.', end='')
                 img = pypdf_ocr.getImagePages(os.path.join(root, f), 1)
                 f_array.append(img['file'])
-                pypdf_ocr.dumpImagePages(img, naming='hin-covers', start=i)
+                pypdf_ocr.dumpImagePages(img, naming='hin-covers', start=i, directory=images_dir)
                 pypdf_ocr.cleanupImageOutput(img)
                 i += 1
+
+    print('\nImages dumped!')
 
     imagesFolded = {
         'tempdir': None,
@@ -48,7 +55,9 @@ def main(naming, directory, AC, part_start=0, part_end=MAX_PARTS):
         'files': f_array
     }
 
-    imagesFolded['images'] = pypdf_ocr.loadImagesToArray(naming='hin-covers', directory='images')
+    print('Reading images to array...')
+    imagesFolded['images'] = pypdf_ocr.loadImagesToArray(naming='hin-covers', directory=images_dir)
+    print('Images read!')
 
     vals = {
         'x': [246, 656, 1022, 1384, 1713, 2090, 2242, 1812, 1800, 1804],
@@ -58,41 +67,49 @@ def main(naming, directory, AC, part_start=0, part_end=MAX_PARTS):
     }
     
     i = 0
+    print('Cropping images', end='')
     for j in range(len(imagesFolded['images'])):
+        print('.', end='')
         copy = imagesFolded['images'][j].copy()
         for k in range(10):        
             pypdf_ocr.generateCropped(copy, i + k, 
                 vals['x'][k],
                 vals['y'][k],
                 vals['w'][k],
-                vals['h'][k])
+                vals['h'][k],
+                directory=images_dir)
         i += 10
+    print('\nImages cropped!')
 
+    print('({}) Doing OCR...'.format(instance))
     # just extract all the cropped pages to english    
     cropped_text_pages = pypdf_ocr.extractPagesText(num_pages=len(imagesFolded['images']) * 10, naming='cropped', language='eng', psm=8,
-            clist='tessedit_char_whitelist=0123456789')
+            clist='tessedit_char_whitelist=0123456789', directory=images_dir)
     # loop to replace the right english attempts with hindi
     for a in range(len(imagesFolded['images'])):
         hi_crop = pypdf_ocr.extractPagesText(num_pages=2, naming='cropped', language='hin', start=10*a+8, psm=8,
-            clist='tessedit_char_blacklist=॥._')
+            clist='tessedit_char_blacklist=॥._', directory=images_dir)
         cropped_text_pages[10*a+8]   = hi_crop[0]
         cropped_text_pages[10*a+8+1] = hi_crop[1]
     
     covers_text_pages = pypdf_ocr.extractPagesText(num_pages=len(imagesFolded['images']), naming='hin-covers', language='hin', psm=4,
-        clist='tessedit_char_blacklist=॥')
+        clist='tessedit_char_blacklist=॥', directory=images_dir)
     
-    pypdf_ocr.dumpTextPages(pages=covers_text_pages, naming='hin-covers')
-    pypdf_ocr.dumpTextPages(pages=cropped_text_pages, naming='cropped')
+    pypdf_ocr.dumpTextPages(pages=covers_text_pages, naming='hin-covers', directory=plaintext_dir)
+    pypdf_ocr.dumpTextPages(pages=cropped_text_pages, naming='cropped', directory=plaintext_dir)
+    print('({}) All OCR dumped!'.format(instance))
 
     part_from_file = re.compile(r'PartNo_(\d{3})')
 
     pages = []
     dicts = []
+    print('Doing regex', end='')
     for j in range(len(covers_text_pages)):
+        print('.', end='')
         page = covers_text_pages[j]
         page1 = pypdf_ocr.extractPage1(page)
         # get it because it's a bunch of crops? lol
-        farm = pypdf_ocr.retreiveCropped(10, start=10*j)
+        farm = pypdf_ocr.retreiveCropped(10, start=10*j, directory=plaintext_dir)
         page1['Sanity'] = farm[0:6]
         page1['Part'] = farm[6]
         page1['Zip Code'] = farm[7]
@@ -105,16 +122,18 @@ def main(naming, directory, AC, part_start=0, part_end=MAX_PARTS):
                 page1['Part'] = int(match.group(1))
         dicts.append(page1)
         pages.append(json.dumps(page1, ensure_ascii=False))
+    print('\nRegex done!')
     
     keys = dicts[0].keys()
-    with open('output.csv', 'w+') as csv_out:
+    with open('output/output{}.csv'.format(AC), 'w+') as csv_out:
         dict_writer = csv.DictWriter(csv_out, keys)
         dict_writer.writeheader()
         dict_writer.writerows(dicts)
 
-    pypdf_ocr.dumpTextPages(pages, naming='hin-covers', directory='output', extension='json')
-    shutil.rmtree('images')
-    shutil.rmtree('plaintext')
+    # pypdf_ocr.dumpTextPages(pages, naming='hin-covers', directory='output', extension='json')
+    shutil.rmtree(images_dir + '/')
+    shutil.rmtree(plaintext_dir + '/')
+    print('Done!')
 
 
 if __name__ == '__main__':
@@ -124,9 +143,11 @@ if __name__ == '__main__':
     parser.add_argument('--AC', type=int, metavar='AC', required=True, help='AC number to process')
     parser.add_argument('--startpart', type=int, help='Which part number to start at')
     parser.add_argument('--endpart', type=int, help='Which part number to end at (max of 999)')
+    parser.add_argument('--instance', type=int, help='Instance for parallel processing')
 
     args = parser.parse_args()
     real_dir = '' if args.dir is None else args.dir
     real_start = 0 if args.startpart is None else args.startpart
-    real_end = MAX_PARTS if args.endpart is None else args.endpart 
-    main(args.filename, real_dir, args.AC, real_start, real_end)
+    real_end = MAX_PARTS if args.endpart is None else args.endpart
+    real_instance = 0 if args.instance is None else args.instance
+    main(args.filename, real_dir, args.AC, real_start, real_end, real_instance)
