e0ea88b95cfc54969d6ace3358a181eec4e9d5a0
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2020-09-21 22:08:25 -0400

    covers_ac_script

--- covers_ac_script.sh
+++ covers_ac_script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# argument 1: number of ACs
-# argument 2: dir
-# argument 3: filename
+# argument 1: start AC
+# argument 2: end AC
+# argument 3: dir
+# argument 4: filename
+# argument 5: instance
 
-
-for i in $(seq 1 $1)
+for i in $(seq $1 $2)
 do
-    python3 process_covers.py --AC $i --filename $3 --dir $2
+    python3 process_covers.py --AC $i --filename $4 --dir $3 --instance $5
+    rm -rf images$5/ plaintext$5/
+    echo "Finished $i"
 done
